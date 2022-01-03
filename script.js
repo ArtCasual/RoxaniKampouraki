dc9c19cba5286f1e3c6ceb1b883f511787b08f72
aristomenisalivanis <aristomenisalivanis@gmail.com>
2022-01-03 22:14:39 +0200

    Js Carousel Added

--- script.js
+++ script.js
@@ -43,15 +43,23 @@ function browserLinks(x){
   }
 };
 
-// carousel component
 
+// carousel component
 const buttons = document.querySelectorAll("[data-button]");
 buttons.forEach(button => {
   button.addEventListener("click", () => {
   const offset = button.dataset.button === "next" ? 1 : -1;
-  const slides = document.querySelectorAll("[data-slides]");
+
+  const slides = document.querySelector("[data-slides");
   const activeSlide = slides.querySelector("[data-active]");
-  let newIndex
+
+  let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+  if (newIndex < 0) newIndex = slides.children.length - 1;
+  if (newIndex >= slides.children.length) newIndex = 0;
+
+  slides.children[newIndex].dataset.active = true;
+  delete activeSlide.dataset.active;
+  
 })
 });
 
