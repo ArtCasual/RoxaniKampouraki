f14b68dda5b37ba1d5d1ac53e8ea746ea2b9a50f
Art.Casua <aristomenisalivanis@gmail.com>
2022-12-12 14:57:56 +0200

    added scroll for mobile and new img

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ closeX.addEventListener("click", () => {
   browserLinks(browserSize);
   browserSize.addEventListener("change", browserLinks);
 });
+
 // resize browser fix
 const browserSize = window.matchMedia("(min-width:700px)");
 function browserLinks(x) {
@@ -40,26 +41,68 @@ function browserLinks(x) {
   }
 }
 
-// carousel component with arrows
+// Touch Event Slider
+let startPos, endPos, offset;
 
+function getPositionX(e) {
+  return e.touches[0].clientX;
+}
+
+function getPositionXEnd(e) {
+  return e.changedTouches[0].clientX;
+}
+function touchStart(e) {
+  startPos = getPositionX(e);
+  return startPos;
+}
+//
+function touchEnd(e) {
+  endPos = getPositionXEnd(e);
+  let distance = endPos - startPos;
+  console.log(distance);
+  if (distance > 70) {
+    offset = 1;
+    touchSLide(offset);
+  } else if (distance < -70) {
+    offset = -1;
+    touchSLide(offset);
+  }
+}
+
+// Carousel component with arrows
+
+const slidesContainer = document.querySelector("[data-slides");
+const slides = [...slidesContainer.children];
+slides.forEach((slide) => {
+  slide.addEventListener("touchstart", touchStart);
+  slide.addEventListener("touchend", touchEnd);
+});
 const buttons = document.querySelectorAll("[data-button]");
-
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    // Next Button plus counter
-    const offset = button.dataset.button === "next" ? 1 : -1;
-
-    const slides = document.querySelector("[data-slides");
-    const activeSlide = slides.querySelector("[data-active]");
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-
-    if (newIndex < 0) newIndex = slides.children.length - 1;
-    if (newIndex >= slides.children.length) newIndex = 0;
-
-    slides.children[newIndex].dataset.active = true;
-    delete activeSlide.dataset.active;
+    changeSlide(button);
   });
 });
+
+// Next Button plus counter
+function touchSLide(offset) {
+  const activeSlide = slidesContainer.querySelector("[data-active]");
+  let newIndex = [...slides].indexOf(activeSlide) + offset;
+  if (newIndex < 0) newIndex = slides.length - 1;
+  if (newIndex >= slides.length) newIndex = 0;
+  slides[newIndex].dataset.active = true;
+  delete activeSlide.dataset.active;
+}
+
+function changeSlide(button) {
+  let offset = button.dataset.button === "next" ? 1 : -1;
+  const activeSlide = slidesContainer.querySelector("[data-active]");
+  let newIndex = [...slides].indexOf(activeSlide) + offset;
+  if (newIndex < 0) newIndex = slides.length - 1;
+  if (newIndex >= slides.length) newIndex = 0;
+  slides[newIndex].dataset.active = true;
+  delete activeSlide.dataset.active;
+}
 
 //Carousel Round Indicators
 
